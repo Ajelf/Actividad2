7e10add67cdd43986ca6794a96e4df816ec55f6d
Ajelf <1232100544@univermilenium.edu.mx>
2024-07-01 22:24:24 -0600

    Add files via upload

---
+++ ACTIVIDAD2/BIFURCACION.js
@@ -0,0 +1,23 @@
+
+// IF
+if (true){
+    console.log("Verdadera");
+}
+
+// IF-ELSE
+if(true){
+    console.log("VERDADERO");
+} else{
+    console.log("falso");
+}
+
+// EJEMPLO USANDO VARIABLES
+
+let edad = 21;
+let legal = 18;
+
+if(edad => legal){
+    console.log("ES LEGAL");
+}else {
+    console.log("ES MENOR DE EDAD");   
+}
---
+++ ACTIVIDAD2/SWITCH.js
@@ -0,0 +1,22 @@
+//switch
+let q1 = 10;
+
+switch (q1) {
+    case 10:
+        console.log("BUEN TRABAJO");
+        break;
+    case 9:
+        console.log ("CASI PERFECTO");
+    case 8:
+        console.log ("BIEN, SUPONGO");
+        break;
+    case 7:
+        console.log ("SIGUE ESTUDIANDO");
+        break;
+    case 6: 
+        console.log ("MEJOR EN ING QUE EN LIC");
+        break;
+    default:
+        console.log("ERROR");
+        break;
+}
