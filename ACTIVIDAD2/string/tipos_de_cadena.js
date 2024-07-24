049a80809f9274ce34bbab24fea23fbde621270b
Ajelf <1232100544@univermilenium.edu.mx>
2024-07-23 18:28:25 -0600

    Add files via upload

---
+++ ACTIVIDAD2/string/tipos_de_cadena.js
@@ -0,0 +1,22 @@
+//string (cadena de caracteres)
+
+let srt_1= "Hola soy un texto con comillas";
+let srt_2= 'Hola soy un texto con comillas simples';
+let srt_3= "Comillas dentro de comillas\"ejemplo\"";
+let srt_4= 'Comillas dentro de comillas"ejemplo"';
+let srt_5="comillas dentro de comillas 'ejemplo'";
+
+console.log(srt_1);
+console.log(srt_2);
+console.log(srt_3);
+console.log(srt_4);
+console.log(srt_5);
+
+//comillas invertidas (backticks)``
+let srt_6 = `comillas invertidas backticks`;
+console.log(srt_6);
+
+let nombre = "Jorge alejandro";
+let welcome= `Hola, ${nombre}, esto es una variable dentro de otro valorr de otra variable`;
+
+console.log(welcome);
